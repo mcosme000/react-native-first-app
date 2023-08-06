1390d12df254f2ef822fc6a61b9566cb4ecd8d34
Maria Cosmen <mcosme00@gmail.com>
2023-08-06 18:18:21 +0900

    App and Main components

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Constants from 'expo-constants'
+import Main from './src/components/Main';
+import RepoList from './src/components/RepoList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{marginTop: Constants.statusBarHeight, padding: "10px"}}>
+      <Main />
+      <RepoList />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
